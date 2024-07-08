42d1e9817afc1dc4f98c0c2d64416f09dbfbcbb6
alvo254 <alvinkhamron@gmail.com>
2024-07-08 23:15:39 +0300

    [works even better]

---
+++ backend/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER ubuntu WITH PASSWORD 'twende@1357';
+    CREATE DATABASE fast_api;
+    GRANT ALL PRIVILEGES ON DATABASE fast_api TO ubuntu;
+EOSQL
