8875a31c44860af7a1beda33b249c66cb37c6184
alvo254 <alvinkhamron@gmail.com>
2024-07-11 00:58:31 +0300

    [update]

--- backend/setup.sh
+++ backend/setup.sh
@@ -4,7 +4,17 @@ set -e
 echo "Starting database setup..."
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER ubuntu WITH PASSWORD 'twende@1357';
+    DO
+    \$do\$
+    BEGIN
+        IF NOT EXISTS (
+            SELECT FROM pg_catalog.pg_roles WHERE rolname = 'ubuntu'
+        ) THEN
+            CREATE USER ubuntu WITH PASSWORD 'twende@1357';
+        END IF;
+    END
+    \$do\$;
+
     GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO ubuntu;
 EOSQL
 
